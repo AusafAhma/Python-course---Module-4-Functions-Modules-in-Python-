fb78c84bac1ecb8c08480ff1ae1f81428d6e97c5
AusafAhma <162278440+AusafAhma@users.noreply.github.com>
2025-10-04 14:22:32 +0800

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,14 +1,8 @@
-def factorial(x):
+def factorial(n):
     result = 1
-    for i in range(1, x + 1):
+    for i in range(1, n + 1):
         result *= i
     return result
-def factorial_recursive(x):
-    if x == 0 or x == 1:
-        return 1
-    else:
-        return x * factorial_recursive(x - 1)
 
-n = int(input("Enter a number: "))
-
-print("Factorial of 5 is:", factorial_recursive(n))
+sample_number = int(input("enter the number:"))
+print(f"The factorial of {sample_number} is: ")
